f57916ba8a50f1c9a41db498be91cc771a90e2bc
unknown <dieimes.nunes@docente.pr.senac.br>
2024-05-02 20:09:13 -0300

    de novo

--- 02_05_2024/js/script.js
+++ 02_05_2024/js/script.js
@@ -1,1 +1,53 @@
-alert("O Palmeiras é campeão 2024 e 2025");
+var nome = "Dieimes";
+
+let sobrenome = "Nunes";
+
+console.log(nome);
+
+console.log(sobrenome);
+
+if(true){
+    var nome2 = "Erica";
+
+}
+
+console.log(nome2);
+
+if(true){
+    let nome3 = "Erica 3";
+
+}
+
+//console.log(nome3);
+
+
+//  let nome = "Matheus"; // String
+
+let numero= 1; // Inteiro
+
+let numero1= 1.15; // Decimais 
+
+let verdade = true; // Booleano
+
+let texto; // Indefino;
+
+let texto02 = null; // Vazio
+
+
+// Adição
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2);
+
+// Subtração
+console.log(num1 - num2);
+
+// Mutiplicação
+console.log(num1 * num2);
+
+// Divição
+console.log(num1 / num2);
+
+
